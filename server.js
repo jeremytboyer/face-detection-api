d390194f6f16263526911bb4a4c7dad1185fc43f
jeremtboyer <jeremy@ableengine.com>
2023-02-10 13:54:13 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { removeAllListeners } = require("nodemon");
+const app = express();
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "test",
+    database: "smart-brain",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => {});
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignIn(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.listen(3001, () => {
+  console.log("app is running on port 3001");
+});
+
+/* 
+/ --> res = working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/user:id --> GET = user
+/image --> PUT --> user
+
+*/
